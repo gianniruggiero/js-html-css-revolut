8c0ea7e27a3fdee8f8ba0441a8f515d320813cf8
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-03 17:43:28 +0200

    test posizione comparsa drop_menu

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(".drop_menu").mouseenter (
   function () {
     $(".menu_panel").hide();
     $(this).children(".menu_panel").show();
+    var menu_left = parseInt($(this).position());
+    $(this).children(".menu_panel").css("left", menu_left);
 })
 
 // $(".drop_menu").mouseleave (
