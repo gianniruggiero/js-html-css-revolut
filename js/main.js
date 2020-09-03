5facefe5173f321595dc2b2453a7463b1db7c8bd
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-03 17:29:38 +0200

    test mouseenter e mouseleave

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-$(".drop_menu").click (
+// $(".drop_menu").click (
+//   function () {
+//     $(this).children(".menu_panel").toggle();
+// })
+
+
+$(".drop_menu").mouseenter (
   function () {
-    $(this).children(".menu_panel").toggle();
+    $(".menu_panel").hide();
+    $(this).children(".menu_panel").show();
+})
+
+// $(".drop_menu").mouseleave (
+//   function () {
+//     $(this).children(".menu_panel").hide();
+// })
+
+$(".menu_panel").mouseleave (
+  function () {
+    $(this).hide();
 })
